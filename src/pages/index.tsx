c96e51345300d6f6a85bf8ebfa61a53ebaa0402f
majselund <majsebergan@gmail.com>
2023-09-02 17:44:44 +0200

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="font-bold text-gray-500 bg-gray-300 h-screen text-9xl justify-center flex items-center">
+      majse
+    </div>
+  )
+}
+
+export default Home
